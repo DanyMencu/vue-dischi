6fa7442cc397f0bd8c1eda3740d95e68c3688ccc
DanyMencu <dany69men@gmail.com>
2021-11-25 14:17:48 +0100

    Create a prototype of album card

--- src/components/AlbumSection.vue
+++ src/components/AlbumSection.vue
@@ -1,5 +1,15 @@
 <template>
-  <section></section>
+    <section>
+        <div class="album-card">
+            <div class="d-flex flex-column align-items-center">
+                <img src="../assets/logo.png" alt="Lorem">
+                <h3>Lorem</h3>
+                <h5>Ipsum</h5>
+                <span>asd</span>
+                <span>asd</span>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
@@ -10,5 +20,15 @@ export default {
 </script>
 
 <style scoped lang="scss">
+    .album-card {
+        width: calc(100% / 8);
+        padding: 5px;
+        margin: 5px 8px;
+        background: #2e3a46;
+
+        img {
+            width: 100%;
+        }
+    }
 
 </style>
